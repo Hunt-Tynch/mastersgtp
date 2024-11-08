70859b6a91c3605400c73082cdcf08a4cd436ac4
Hunt-Tynch <129477367+Hunt-Tynch@users.noreply.github.com>
2024-11-07 22:32:48 -0500

    Delete mastersgtp-frontend directory

--- mastersgtp-frontend/src/service/ScratchService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const BASE_URL = 'http://localhost:8080/api/scratch'
-
-export const createScratch = (scratch) => axios.post(BASE_URL, scratch)
-
-export const deleteScratch = (id) => axios.delete(`${BASE_URL}/${id}`)
-
-export const getScratches = () => axios.get(BASE_URL)
